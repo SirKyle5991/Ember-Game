c31beb119e1ccd2bd58b124e1d620c54656e69e8
SirKyle5991 <kylos.venguard@hotmail.com>
2026-03-30 11:22:24 +0800

    added player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private void Awake()
+    {
+        // grab references for rigidBody and animator for the object
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //flips the player when moving left and right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+
+        //set animator parameters
+        anim.SetBool("Run", horizontalInput != 0);
+    }
+}
---
+++ Assets/Scripts/WallSconce.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSconce : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.GetComponent<PlayerController>())
+        {
+            Debug.Log("i think you hit something");
+        }
+    }
+}
