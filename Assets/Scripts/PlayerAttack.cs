b6d98c5a81ed1d0661e5744f69756e44e172aa07
SirKyle5991 <kylos.venguard@hotmail.com>
2026-04-12 23:19:24 +0800

    concerning flameburst
    attempted at creating scripts for flameburst while also adding assets for art.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private Animator anim;
+    private PlayerController playerController;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMovement(0) && cooldownTimer > attackCooldown && playerController.canAttack())
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+}
