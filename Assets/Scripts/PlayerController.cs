912c01ee6502e7ca31205ad7bfe0f2bbc8314369
SirKyle5991 <kylos.venguard@hotmail.com>
2026-04-15 21:01:44 +0800

    tested jump animation with placeholders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
@@ -27,10 +28,23 @@ public class PlayerController : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
         //set animator parameters
         anim.SetBool("Run", horizontalInput != 0);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, 5);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
